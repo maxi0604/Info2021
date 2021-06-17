b381682fbf1876259324dbec1f68ce5715c8f92a
FightingFights <superm1000@outlook.de>
2021-06-17 17:16:21 +0200

    Kommentiert

--- Player.cs
+++ Player.cs
@@ -7,8 +7,10 @@ namespace Info2021 {
 
         public Vector2 Position => velPos.P;
         public void Update(float dt) {
-
+            // Gravity
             velPos = velPos.Accelerate(new Vector2(0, 9.81f));
+
+            // Movement logic
             bool directionalMovement = true;
             if (Keyboard.GetState().IsKeyDown(Keys.Right))
                 velPos = velPos.Accelerate(new Vector2(40f, 0));
@@ -17,16 +19,18 @@ namespace Info2021 {
             else
                 directionalMovement = false;
             
+            // Air resistance
             if (directionalMovement)
                 velPos = velPos.Accelerate(new Vector2(-0.1f * velPos.V.X, 0));
             else
                 velPos = velPos.Accelerate(new Vector2(-0.5f * velPos.V.X, 0));
 
-
+            // Ground test. Remove this
             if (velPos.P.Y > 200) {
                 velPos = velPos.Accelerate(new Vector2(0, -velPos.V.Y));
             }
 
+            // Finalize physics by actually changing the position
             velPos = velPos.ApplyVelocity(dt);
         }
     }
