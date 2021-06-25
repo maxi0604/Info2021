4c60383d934ec9770d452af96adb32947922c46e
uni <arvid.marx@protonmail.com>
2021-06-25 18:20:52 +0200

    level fix

--- FirstLevel.cs
+++ FirstLevel.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.IO;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 namespace Info2021
@@ -9,30 +10,31 @@ namespace Info2021
             List<Tile> tiles = new List<Tile>();
             List<StaticCollider> staticColliders = new List<StaticCollider>();
             for(int i = 0; i < 30; i++) {
-                tiles.Add(new Tile(new TileInfo(game.resourceAccessor, 2, 0), i, 15));
+                tiles.Add(new Tile(new TileInfo(2, 0), i, 15));
                 
             }
              for(int i = 0; i < 30; i++) {
-                tiles.Add(new Tile(new TileInfo(game.resourceAccessor, 0, 2), 30, i));
+                tiles.Add(new Tile(new TileInfo(0, 2), 30, i));
                 
             }
             for(int i = 12; i < 15; i++) {
                 Tile tile;
                 StaticCollider staticCollider;
-                (tile, staticCollider) = GroundHelper(new TileInfo(game.resourceAccessor, 17, 0), 12, i);
+                (tile, staticCollider) = GroundHelper(new TileInfo(17, 0), 12, i);
                 tiles.Add(tile);
                 staticColliders.Add(staticCollider);
             }
             Tile tile1;
             MovingPlatform platform = new MovingPlatform(new Vector2(22*16, 12*16), new Vector2(-4*16,0), 3);
             StaticCollider staticCollider1;
-            (tile1, staticCollider1) = GroundHelper(new TileInfo(game.resourceAccessor, 17, 0), 14, 11);
+            (tile1, staticCollider1) = GroundHelper(new TileInfo(17, 0), 14, 11);
             staticColliders.Add(staticCollider1);
             tiles.Add(tile1);
             staticColliders.Add(new StaticCollider(new Vector2(0, 15*16), new Vector2(450, 16*16)));
             staticColliders.Add(new StaticCollider(new Vector2(30*16, 0), new Vector2(31*16, 450)));
             
-
+            //
+            //tiles = BinarySerializer.Deserialize<List<Tile>>(File.OpenRead("Czfhfdsh"));
             Level a = new Level(Vector2.Zero, Vector2.Zero, tiles, staticColliders, new List<DynamicObject>(),
             new List<CinematicObject>() {
                 new Spikes(new Vector2(5*16,14*16), 3),
@@ -43,8 +45,9 @@ namespace Info2021
                 new Spring(new Vector2(20*16, 12*16), 0),
                 new Spring(new Vector2(19*16, 14*16), 3),                
                 },
-                new Background(game.resourceAccessor.LoadContent<Texture2D>("background1")));
+                new Background("background1"));
             platform.Add(a);
+            BinarySerializer.Serialize<Level>(a, File.OpenWrite("testlevel"));
             return a;
         }
 
