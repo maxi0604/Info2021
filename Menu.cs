d25b31c50207aafa212bdb94e953e45308ff0096
FightingFights <superm1000@outlook.de>
2021-06-25 22:32:50 +0200

    small refactor

--- Menu.cs
+++ Menu.cs
@@ -5,7 +5,7 @@ namespace Info2021
     
     class Menu : AbstractMenu<MenuItem>
     {
-        public override MenuItem[] AllItems {get {return new MenuItem[] {MenuItem.LevelSelect, MenuItem.LevelEdit, MenuItem.Settings, MenuItem.Exit}; }}
+        public override MenuItem[] AllItems { get => new  MenuItem[] {MenuItem.LevelSelect, MenuItem.LevelEdit, MenuItem.Settings, MenuItem.Exit}; }
         
 
         public Menu(SpriteBatch spriteBatch, ResourceAccessor resourceAccessor) : base(spriteBatch, resourceAccessor)
