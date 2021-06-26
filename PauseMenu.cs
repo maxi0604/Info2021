717bb439acbfc49c6efc60ace8682704ab3bedcd
FightingFights <superm1000@outlook.de>
2021-06-26 14:42:20 +0200

    small syntax change

--- PauseMenu.cs
+++ PauseMenu.cs
@@ -5,7 +5,7 @@ namespace Info2021
     
     class PauseMenu : AbstractMenu<PauseMenuItem>
     {
-        public override PauseMenuItem[] AllItems {get {return new PauseMenuItem[] {PauseMenuItem.Unpause, PauseMenuItem.Retry, PauseMenuItem.Settings, PauseMenuItem.MainMenu}; }}
+        public override PauseMenuItem[] AllItems {get => new PauseMenuItem[] {PauseMenuItem.Unpause, PauseMenuItem.Retry, PauseMenuItem.Settings, PauseMenuItem.MainMenu}; }
         
 
         public PauseMenu(SpriteBatch spriteBatch, ResourceAccessor resourceAccessor) : base(spriteBatch, resourceAccessor)
