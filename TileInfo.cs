e29c1f9ad45d856f1f1c5bb1558a1ecf33130fd1
uni <arvid.marx@protonmail.com>
2021-06-27 20:45:54 +0200

    removed unnecessary code

--- TileInfo.cs
+++ TileInfo.cs
@@ -11,23 +11,6 @@ namespace Info2021
         public float layer;
         [DataMember]
         public int width, height, tx, ty;
-        /*
-        public TileInfo(ResourceAccessor resourceAccessor, string texturePath, float layer, int width, int height) : 
-            this(resourceAccessor.LoadContent<Texture2D>(texturePath), layer, width, height)
-            { }
-        public TileInfo(ResourceAccessor resourceAccessor, string texturePath, float layer) : 
-            this(resourceAccessor.LoadContent<Texture2D>(texturePath), layer, 0, 0) {
-                height = Texture.Height;
-                width = Texture.Width;
-        }
-        public TileInfo(ResourceAccessor resourceAccessor, string texturePath, int width, int height) :
-            this(resourceAccessor.LoadContent<Texture2D>(texturePath), 0.1f, width, height) { }
-        public TileInfo(ResourceAccessor resourceAccessor, string texturePath) : 
-            this(resourceAccessor.LoadContent<Texture2D>(texturePath), 0.1f, 0, 0) {
-                height = Texture.Height;
-                width = Texture.Width;
-        }
-        */ 
         public TileInfo(int x, int y) : this(0.1f, 16, 16, x, y) {
 
         }
